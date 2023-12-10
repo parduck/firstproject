07bd4cb152533bd26d9f1357aab604fa0388f674
chankim <videoal@gmail.com>
2023-12-10 22:17:43 +0900

    dto to database save

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ springboot : https://start.spring.io/
 bootStrap : https://getbootstrap.com
 
 gitignore 적용 : git rm -r --cached . 
+
+인텔리제이 한글깨짐
+- file-settings editor>File Encodings에서 Global Encoding, Project Encoding, Default encoding for properties files를 UTF-8
+- Help = Edit custom VM options >> "-Dfile.encoding=UTF-8" 추가
--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,12 +1,19 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+
+    //의존성을 주입시키는걸 스프링부트가 해줘서 별도 구현체를 만드는 과정을 생략한다.
+    @Autowired
+    private ArticleRepository articleRepository;
     @GetMapping("/articles/new")
     public String newArticleForm(){
         return "articles/new";
@@ -15,6 +22,14 @@ public class ArticleController {
     @PostMapping("articles/create")
     public String createArticle(ArticleForm form){
         System.out.println(form.toString());
+        //1. DTO를 엔티티로 변화
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+
+        //2. 리파지터리로 엔티티를 DB에 저장
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
+
         return "articles/new";
     }
 }
